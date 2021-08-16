2dffaafed8e37083f78a1a1b2ef57cf8516ad21c
Lucero-L <81035584+Lucero-L@users.noreply.github.com>
2021-08-16 00:54:52 -0500

    Add files via upload

---
+++ Invoice/InvoiceUnach/Invoice.Domain/Entities/UserRol.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Invoice.Domain.Exceptions;
+using Invoice.Domain.SeedWork;
+
+namespace Invoice.Domain.Entities
+{
+    public class UserRol : BaseEntity
+    {
+        #region Constructor & properties
+
+        public string RolName { get; private set; }
+        public Guid UserId { get; private set; }
+
+        protected UserRol()
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public UserRol(string rolName, Guid userId)
+        {
+            SetRolName(rolName);
+            SetUserId(userId);
+        }
+
+        #endregion
+
+        #region Setters
+
+        public void SetRolName(string value)
+        {
+            if (string.IsNullOrEmpty(value)) throw new InvoiceDomainException("The name is required.");
+
+            RolName = value;
+        }
+
+      public void SetUserId(Guid value)
+        {
+            if (value==null) throw new InvoiceDomainException("The code catalog is required.");
+
+            UserId = value;
+          
+        }
+
+       
+        #endregion
+    }
+}
