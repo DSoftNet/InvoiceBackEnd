94e58eb850b2272a2514196d049feda53ac81cca
lidia <lidia.enriquez@unach.edu.ec>
2021-08-15 22:04:56 -0500

    add entity subsidiary

---
+++ Invoice/InvoiceUnach/Invoice.UnitTests/Domain/Entities/CatalogDomainTests.cs
@@ -0,0 +1,27 @@
+﻿using Invoice.Domain.Entities;
+using Invoice.Domain.Exceptions;
+using Xunit;
+
+namespace Invoice.UnitTests.Domain.Entities
+{
+    public class CatalogDomainTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Constructor_NameIsInvalid_ThrowInvoiceDomainException(string name)
+        {
+            Assert.Throws<InvoiceDomainException>(() =>
+                new Catalog(name, "code", "value", "description", true));
+        }
+        
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Constructor_CodeIsInvalid_ThrowInvoiceDomainException(string code)
+        {
+            Assert.Throws<InvoiceDomainException>(() =>
+                new Catalog("name", code, "value", "description", true));
+        }
+    }
+}
