8017d39ca7e1acb2cafe4585076db97d75585f33
Alex Asitimbay <alexasitimbay@outlook.com>
2021-08-19 21:31:25 -0500

    clean code

--- Invoice/InvoiceUnach/Invoice.UnitTests/Domain/Entities/ClientDomainTests.cs
+++ Invoice/InvoiceUnach/Invoice.UnitTests/Domain/Entities/ClientDomainTests.cs
@@ -12,70 +12,74 @@ namespace Invoice.UnitTests.Domain.Entities
         [InlineData(null)]
         public void Constructor_firstNameIsInvalid_ThrowInvoiceDomainException(string firstName)
         {
-            Assert.Throws<InvoiceDomainException>(testCode:()=>
-                new Client(firstName, secondName: "secondName", firstLastName: "firstLastName",
-                    secondLastName: "secondLastName", identificationType: "identificationType",
-                    identification: "identification", email: "email", address: "address", phone: "phone",
-                    cellPhone: "cellPhone", status: true, registrationDate: DateTime.Now,
-                    updateDate: DateTime.Now, userId: Guid.Parse("5C60F693-BEF5-E011-A485-80EE7300C695")));
+            Assert.Throws<InvoiceDomainException>(() =>
+                new Client(firstName, "secondName", "firstLastName",
+                    "secondLastName", "identificationType",
+                    "identification", "email", "address", "phone",
+                    "Phone", status: true, Guid.Parse("5C60F693-BEF5-E011-A485-80EE7300C695")));
         }
+
         [Theory]
         [InlineData("")]
         [InlineData(null)]
         public void Constructor_firstLastNameIsInvalid_ThrowInvoiceDomainException(string firstLastName)
         {
-            Assert.Throws<InvoiceDomainException>(testCode:()=>
+            Assert.Throws<InvoiceDomainException>(() =>
                 new Client(firstName: "firstName", secondName: "secondName", firstLastName,
-                    secondLastName: "secondLastName", identificationType: "identificationType",
-                    identification: "identification", email: "email", address: "address", phone: "phone",
-                    cellPhone: "cellPhone", status: true, registrationDate: DateTime.Now,
-                    updateDate: DateTime.Now, userId: Guid.Parse("5C60F693-BEF5-E011-A485-80EE7300C695")));
+                    "secondLastName", "identificationType",
+                    "identification", "email", "address", "phone",
+                    "Phone", status: true,
+                    Guid.Parse("5C60F693-BEF5-E011-A485-80EE7300C695")));
         }
+
         [Theory]
         [InlineData("")]
         [InlineData(null)]
         public void Constructor_identificationType_ThrowInvoiceDomainException(string identificationType)
         {
-            Assert.Throws<InvoiceDomainException>(testCode:()=>
-                new Client(firstName: "firstName", secondName: "secondName", firstLastName:"firstLastName",
-                    secondLastName: "secondLastName", identificationType,
-                    identification: "identification", email: "email", address: "address", phone: "phone",
-                    cellPhone: "cellPhone", status: true, registrationDate: DateTime.Now,
-                    updateDate: DateTime.Now, userId: Guid.Parse("5C60F693-BEF5-E011-A485-80EE7300C695")));
+            Assert.Throws<InvoiceDomainException>(() =>
+                new Client(firstName: "firstName", secondName: "secondName", "firstLastName",
+                    "secondLastName", identificationType,
+                    "identification", "email", "address", "phone",
+                    "Phone", status: true,
+                    Guid.Parse("5C60F693-BEF5-E011-A485-80EE7300C695")));
         }
+
         [Theory]
         [InlineData("")]
         [InlineData(null)]
         public void Constructor_identification_ThrowInvoiceDomainException(string identification)
         {
-            Assert.Throws<InvoiceDomainException>(testCode:()=>
-                new Client(firstName: "firstName", secondName: "secondName", firstLastName:"firstLastName",
-                    secondLastName: "secondLastName", identificationType: "identificationType",
-                    identification, email: "email", address: "address", phone: "phone",
-                    cellPhone: "cellPhone", status: true, registrationDate: DateTime.Now,
-                    updateDate: DateTime.Now, userId: Guid.Parse("5C60F693-BEF5-E011-A485-80EE7300C695")));
+            Assert.Throws<InvoiceDomainException>(() =>
+                new Client(firstName: "firstName", secondName: "secondName", "firstLastName",
+                    "secondLastName", "identificationType",
+                    identification, "email", "address", "phone",
+                    "Phone", status: true,
+                    Guid.Parse("5C60F693-BEF5-E011-A485-80EE7300C695")));
         }
+
         [Theory]
         [InlineData("")]
         [InlineData(null)]
         public void Constructor_email_ThrowInvoiceDomainException(string email)
         {
-            Assert.Throws<InvoiceDomainException>(testCode:()=>
-                new Client(firstName: "firstName", secondName: "secondName", firstLastName:"firstLastName",
-                    secondLastName: "secondLastName", identificationType: "identificationType",
-                    identification: "identification", email, address: "address", phone: "phone",
-                    cellPhone: "cellPhone", status: true, registrationDate: DateTime.Now,
-                    updateDate: DateTime.Now, userId: Guid.Parse("5C60F693-BEF5-E011-A485-80EE7300C695")));
+            Assert.Throws<InvoiceDomainException>(() =>
+                new Client(firstName: "firstName", secondName: "secondName", "firstLastName",
+                    "secondLastName", "identificationType",
+                    "identification", email, "address", "phone",
+                    "Phone", status: true,
+                    Guid.Parse("5C60F693-BEF5-E011-A485-80EE7300C695")));
         }
-        [Fact]    
+
+        [Fact]
         public void Constructor_userId_ThrowInvoiceDomainException()
         {
-            Assert.Throws<InvoiceDomainException>(testCode:()=>
-                new Client(firstName: "firstName", secondName: "secondName", firstLastName:"firstLastName",
-                    secondLastName: "secondLastName", identificationType: "identificationType",
-                    identification: "identification", email:"email", address: "address", phone: "phone",
-                    cellPhone: "cellPhone", status: true, registrationDate: DateTime.Now,
-                    updateDate: DateTime.Now, Guid.Empty));
+            Assert.Throws<InvoiceDomainException>(() =>
+                new Client(firstName: "firstName", secondName: "secondName", "firstLastName",
+                    "secondLastName", "identificationType",
+                    "identification", email: "email", "address", "phone",
+                    "Phone", status: true,
+                    Guid.Empty));
         }
     }
 }
