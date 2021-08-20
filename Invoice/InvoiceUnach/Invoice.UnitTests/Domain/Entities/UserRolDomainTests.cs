ffee8fcb5602d63443154366e688f8eaecee4442
luis <fernando2020lucero@gmail.com>
2021-08-19 21:40:43 -0500

    UserRol Unit Tests

---
+++ Invoice/InvoiceUnach/Invoice.UnitTests/Domain/Entities/UserRolDomainTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Invoice.Domain.Entities;
+using Invoice.Domain.Exceptions;
+using Xunit;
+
+namespace Invoice.UnitTests.Domain.Entities
+{
+    public class UserRolDomainTests
+    {
+        [Theory]
+        [InlineData("")]
+        [InlineData(null)]
+        public void Constructor_firstNameIsInvalid_ThrowInvoiceDomainException(string rolName)
+        {
+            Assert.Throws<InvoiceDomainException>(() =>
+                new UserRol(rolName, Guid.NewGuid()));
+        }
+        
+    }
+}
