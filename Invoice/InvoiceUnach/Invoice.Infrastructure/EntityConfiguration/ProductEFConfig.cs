93659b4c212a78ca0c31e60089c8e8aee5309626
lidia <lidia.enriquez@unach.edu.ec>
2021-08-20 16:42:58 -0500

    configuration of ItemCatalogRepositoy

--- Invoice/InvoiceUnach/Invoice.Infrastructure/EntityConfiguration/ProductEFConfig.cs
+++ Invoice/InvoiceUnach/Invoice.Infrastructure/EntityConfiguration/ProductEFConfig.cs
@@ -16,45 +16,39 @@ namespace Invoice.Infrastructure.EntityConfiguration
                 .HasMaxLength(100)
                 .IsRequired();
 
-            builder.Property(x => x.Desciption)
-                .HasMaxLength(255)
+            builder.Property(x => x.Description)
+                .HasMaxLength(255);
 
             builder.Property(x => x.Code)
                 .HasMaxLength(30)
                 .IsRequired();
 
             builder.Property(x => x.Price)
-                .HasMaxLength()
+                .HasMaxLength(100);
 
-            builder.Property(x => x.Islva)
-                .HasMaxLength()
+            builder.Property(x => x.IsIva)
+                .HasMaxLength(100);
 
             builder.Property(x => x.Stock)
-                .HasMaxLength()
+
                 .IsRequired();
 
             builder.Property(x => x.IsExpiration)
-                .HasMaxLength()
+                .HasMaxLength(10);
         
-            builder.Property(x => x.Expiration)
+            builder.Property(x => x.ExpirationAt)
                 .IsRequired();
-            
-            builder.Property(x => x.Status)
-                .HasMaxLength()
 
+            builder.Property(x => x.Status);
+                
             builder.Property(x => x.CreateAt)
                 .IsRequired();
 
             builder.Property(x => x.UpdateAt)
                 .IsRequired();
-            
+
             builder.Property(x => x.UserId)
                 .IsRequired();
-
-            builder.HasIndex(x => new {x.UserId})
-                .IsUnique();
-
-
         }
     }
     
