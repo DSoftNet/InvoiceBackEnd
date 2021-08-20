2340c46a167fd54d2420407070f2c197af561285
gandhy garcia <gandhy.garcia@unach.edu.ec>
2021-08-20 14:53:29 -0500

    UserRol repository creation

---
+++ Invoice/InvoiceUnach/Invoice.Domain/Interfaces/Repositories/IUserRolRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Invoice.Domain.Entities;
+using Invoice.Domain.SeedWork;
+
+namespace Invoice.Domain.Interfaces.Repositories
+{
+    public interface IUserRolRepository: IRepository<UserRol>
+    {
+        Task<List<UserRol>> Gets();
+        Task<UserRol> Get(Guid id);
+        UserRol Add(UserRol userRol);
+        UserRol Update(UserRol userRol);
+    }
+}
