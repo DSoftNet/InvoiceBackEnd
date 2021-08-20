368a1917b4787f25793d36b58c1912b849a5bbcc
Alex Asitimbay <alexasitimbay@outlook.com>
2021-08-19 23:13:27 -0500

    Configuration of repository Pattern

---
+++ Invoice/InvoiceUnach/Invoice.Domain/Interfaces/Repositories/ICatalogRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Invoice.Domain.Entities;
+using Invoice.Domain.SeedWork;
+
+namespace Invoice.Domain.Interfaces.Repositories
+{
+    public interface ICatalogRepository : IRepository<Catalog>
+    {
+        Task<List<Catalog>> Gets();
+        Task<Catalog> Get(Guid id);
+        Catalog Add(Catalog catalog);
+        Catalog Update(Catalog catalog);
+    }
+}
