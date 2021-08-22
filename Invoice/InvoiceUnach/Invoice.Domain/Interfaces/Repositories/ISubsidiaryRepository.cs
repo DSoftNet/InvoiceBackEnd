5ad0757e8191884a81c887c8547405f88f01f3c0
luis <fernando2020lucero@gmail.com>
2021-08-21 19:40:27 -0500

    Create Repository Subsidiary

---
+++ Invoice/InvoiceUnach/Invoice.Domain/Interfaces/Repositories/ISubsidiaryRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Invoice.Domain.Entities;
+using Invoice.Domain.SeedWork;
+
+
+namespace Invoice.Domain.Interfaces.Repositories
+{
+    public interface ISubsidiaryRepository: IRepository<Subsidiary>
+    {
+        Task<List<Subsidiary>> Gets();
+        Task<Subsidiary> Get(Guid id);
+        Subsidiary Add(Subsidiary subsidiary);
+        Subsidiary Update(Subsidiary subsidiary);
+    }
+}
---
+++ Invoice/InvoiceUnach/Invoice.Infrastructure/Repositories/SubsidiaryRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Invoice.Domain.Entities;
+using Invoice.Domain.Interfaces.Repositories;
+using Invoice.Domain.SeedWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace Invoice.Infrastructure.Repositories
+{
+    public class SubsidiaryRepository : ISubsidiaryRepository
+    {
+        public IUnitOfWork UnitOfWork => _dbContext;
+        private readonly InvoiceDbContext _dbContext;
+
+        public SubsidiaryRepository(InvoiceDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<List<Subsidiary>> Gets()
+        {
+            return await _dbContext.Subsidiaries.ToListAsync();
+        }
+
+        public async Task<Subsidiary> Get(Guid id)
+        {
+            return await _dbContext.Subsidiaries.FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public Subsidiary Add(Subsidiary subsidiary)
+        {
+            return _dbContext.Subsidiaries.Add(subsidiary).Entity;
+        }
+
+        public Subsidiary Update(Subsidiary subsidiary)
+        {
+            return _dbContext.Subsidiaries.Update(subsidiary).Entity;
+        }
+    }
+}
