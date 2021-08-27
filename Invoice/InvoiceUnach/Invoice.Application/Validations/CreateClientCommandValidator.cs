02e4c7ba9187d0f81b3d45ec6c9f8e404cb86998
Alex Asitimbay <alexasitimbay@outlook.com>
2021-08-26 22:48:39 -0500

    fix clean code in unit tes of client

--- Invoice/InvoiceUnach/Invoice.Application/Validations/CreateClientCommandValidator.cs
+++ Invoice/InvoiceUnach/Invoice.Application/Validations/CreateClientCommandValidator.cs
@@ -9,26 +9,34 @@ namespace Invoice.Application.Validations
         {
             RuleFor(t => t.FirstName)
                 .NotEmpty();
-            
+
             RuleFor(t => t.SecondName)
                 .NotEmpty();
-            
+
             RuleFor(t => t.FirstLastName)
                 .NotEmpty();
+
             RuleFor(t => t.SecondLastName)
                 .NotEmpty();
+
             RuleFor(t => t.IdentificationType)
                 .NotEmpty();
+
             RuleFor(t => t.Identification)
                 .NotEmpty();
+
             RuleFor(t => t.Email)
                 .NotEmpty();
+
             RuleFor(t => t.Address)
                 .NotEmpty();
+
             RuleFor(t => t.Phone)
                 .NotEmpty();
+
             RuleFor(t => t.CellPhone)
                 .NotEmpty();
+
             RuleFor(t => t.UserId)
                 .NotEmpty();
         }
--- Invoice/InvoiceUnach/Invoice.UnitTests/Application/Commands/CreateClientCommandHandlerTests.cs
+++ Invoice/InvoiceUnach/Invoice.UnitTests/Application/Commands/CreateClientCommandHandlerTests.cs
@@ -17,35 +17,33 @@ namespace Invoice.UnitTests.Application.Commands
     public class CreateClientCommandHandlerTests
     {
         private readonly Mock<ILogger<CreateClientCommandHandler>> _logger;
-        private readonly Mock<ICatalogRepository> _catalogRepository;
         private readonly Mock<IUnitOfWork> _dbContext;
-        private readonly Mock<IMediator> _mediador;
-        
-        private readonly CreateClientCommandHandler _handler;
         private readonly Mock<IClientRepository> _clientRepository;
+        private readonly Mock<IMediator> _mediator;
+
+        private readonly CreateClientCommandHandler _handler;
 
         public CreateClientCommandHandlerTests()
         {
             _logger = new Mock<ILogger<CreateClientCommandHandler>>();
             _clientRepository = new Mock<IClientRepository>();
             _dbContext = new Mock<IUnitOfWork>();
+            _mediator = new Mock<IMediator>();
+
             _clientRepository.Setup(x => x.UnitOfWork)
                 .Returns(_dbContext.Object);
-            var _mediator = new Mock<IMediator>();
-            _mediator.Setup(x => x.Send(new ValidateUserService(It.IsAny<Guid>()),
-                default)).ReturnsAsync(true);
-            
-            _handler = new CreateClientCommandHandler(_logger.Object, _clientRepository.Object,_mediator.Object);
+
+            _handler = new CreateClientCommandHandler(_logger.Object, _clientRepository.Object, _mediator.Object);
         }
-    
+
         [Fact]
         public async Task Handle_EmailIsNotExist_ReturnTrue()
         {
-            var command = new CreateClientCommand("firstname","secondName","firstLastName",
-                "secondLastName","identificationType","identification","email",
-                "address","phone","cellphone",true,DateTime.Now, DateTime.Now,
+            var command = new CreateClientCommand("firstname", "secondName", "firstLastName",
+                "secondLastName", "identificationType", "identification", "email",
+                "address", "phone", "cellphone", true, DateTime.Now, DateTime.Now,
                 Guid.NewGuid());
-            
+
             _clientRepository.Setup(x => x.Get(It.IsAny<string>()))
                 .ReturnsAsync(null as Client);
 
@@ -55,57 +53,56 @@ namespace Invoice.UnitTests.Application.Commands
             //Assert
             Assert.True(actual);
         }
-        
+
         [Fact]
         public async Task Handle_EmailIsNotExist_AddCalled()
         {
-            var command = new CreateClientCommand("firstname","secondName","firstLastName",
-                "secondLastName","identificationType","identification","email",
-                "address","phone","cellphone",true,DateTime.Now, DateTime.Now,
+            var command = new CreateClientCommand("firstname", "secondName", "firstLastName",
+                "secondLastName", "identificationType", "identification", "email",
+                "address", "phone", "cellphone", true, DateTime.Now, DateTime.Now,
                 Guid.NewGuid());
 
             //Act
             bool actual = await _handler.Handle(command, default);
 
             //Assert
-            _clientRepository.Verify(x=>x.Add(
-                It.IsAny<Client>()),Times.Once);
+            _clientRepository.Verify(x => x.Add(
+                It.IsAny<Client>()), Times.Once);
         }
-        
+
         [Fact]
         public async Task Handle_EmailIsNotExist_SaveEntitiesAsync()
         {
-            var command = new CreateClientCommand("firstname","secondName","firstLastName",
-                "secondLastName","identificationType","identification","email",
-                "address","phone","cellphone",true,DateTime.Now, DateTime.Now,
+            var command = new CreateClientCommand("firstname", "secondName", "firstLastName",
+                "secondLastName", "identificationType", "identification", "email",
+                "address", "phone", "cellphone", true, DateTime.Now, DateTime.Now,
                 Guid.NewGuid());
 
             //Act
             await _handler.Handle(command, default);
 
             //Assert
-            _dbContext.Verify(x=>x.SaveEntitiesAsync(It.IsAny<CancellationToken>()),
+            _dbContext.Verify(x => x.SaveEntitiesAsync(It.IsAny<CancellationToken>()),
                 Times.Once);
         }
-        
+
         [Fact]
-        public async Task Handle_EmailIsNotExist_ThrowException()
+        public async Task Handle_EmailExist_ThrowException()
         {
-            var command = new CreateClientCommand("firstname","secondName","firstLastName",
-                "secondLastName","identificationType","identification","email",
-                "address","phone","cellphone",true,DateTime.Now, DateTime.Now,
+            var command = new CreateClientCommand("firstname", "secondName", "firstLastName",
+                "secondLastName", "identificationType", "identification", "email",
+                "address", "phone", "cellphone", true, DateTime.Now, DateTime.Now,
                 Guid.NewGuid());
 
             var dbClient = new Mock<Client>();
             dbClient.Object.SetEmail("email");
-            
+
             _clientRepository.Setup(x => x.Get(It.IsAny<string>()))
                 .ReturnsAsync(dbClient.Object);
-            
+
             //Assert
-            await Assert.ThrowsAsync<InvoiceDomainException>(()=> _handler.Handle(command,
+            await Assert.ThrowsAsync<InvoiceDomainException>(() => _handler.Handle(command,
                 default));
         }
-
     }
 }
