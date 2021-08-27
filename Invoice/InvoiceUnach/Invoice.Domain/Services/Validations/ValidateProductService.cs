557324791d9ead5263ce3ef07294a6acfc232299
gandhy garcia <gandhy.garcia@unach.edu.ec>
2021-08-26 22:27:41 -0500

    creation validate product

---
+++ Invoice/InvoiceUnach/Invoice.Domain/Services/Validations/ValidateProductService.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace Invoice.Domain.Services.Validations
+{
+    public class ValidateProductService : IRequest<bool>
+    {
+        public string Code { get; private set; }
+
+        public ValidateProductService(string code)
+        {
+            Code = code;
+        }
+    }
+}
