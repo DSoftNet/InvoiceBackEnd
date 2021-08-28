f7e6ec44e722af31f4397227e338ac9b81e6ed38
ClintonMoina <moinajamil15@gmail.com>
2021-08-27 21:16:25 -0500

    create query client

---
+++ Invoice/InvoiceUnach/Invoice.Application/Queries/ReadClientsQuery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using Invoice.Application.Dtos.Responses;
+using MediatR;
+
+namespace Invoice.Application.Queries
+{
+    public class ReadClientsQuery : IRequest<ClientResponse>, IRequest<List<ClientResponse>>
+    {
+        
+        public Guid UserId{ get; private set; }
+
+        public ReadClientsQuery(Guid userId)
+        {
+            UserId = userId;
+        }
+        
+    }
+}
---
+++ Invoice/InvoiceUnach/Invoice.Application/Queries/ReadClientsQueryHandler.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Invoice.Application.Dtos.Responses;
+using Invoice.Domain.Interfaces.Repositories;
+using Invoice.Domain.Services.Validations;
+using MediatR;
+using Microsoft.Extensions.Logging;
+
+namespace Invoice.Application.Queries
+{
+    public class ReadClientsQueryHandler : IRequestHandler<ReadClientsQuery, List<ClientResponse>>
+    {
+        private readonly ILogger<ReadClientsQueryHandler> _logger;
+        private readonly IClientRepository _clientRepository;
+    
+
+        public ReadClientsQueryHandler(ILogger<ReadClientsQueryHandler> logger, IClientRepository clientRepository)
+        {
+            _logger = logger;
+            _clientRepository = clientRepository;
+        }
+
+        public async Task<List<ClientResponse>> Handle(ReadClientsQuery query, CancellationToken cancellationToken)
+        {
+            var clients = await _clientRepository.Gets();
+
+            /*return  clients.select(t => new ClientResponse(t.Id, t.firsName, t.secondName, t.firsLastName, 
+                t.secondLastName,t.identificationType, t.identificationType, t.email, t.address, t.phone,
+                t.cellPhone, t.status, t.userId)).ToList();*/
+            return clients.Select(t => new ClientResponse(t.Id, t.FirstName, t.SecondName, t.FirstLastName, 
+                t.SecondLastName, t.IdentificationType, t.Identification, t.Email, t.Address, t.Phone,
+                t.CellPhone, t.Status, t.UserId)).ToList();
+        }
+    }
+}
