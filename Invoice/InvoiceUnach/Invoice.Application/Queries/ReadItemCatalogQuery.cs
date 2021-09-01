277164e72377267b942a37b22c54e66da08b0576
lidia <lidia.enriquez@unach.edu.ec>
2021-08-31 20:54:30 -0500

    add-Controllers-Queries of itemCatalog

---
+++ Invoice/InvoiceUnach/Invoice.Application/Queries/ReadItemCatalogQuery.cs
@@ -0,0 +1,19 @@
+﻿using Invoice.Application.Dtos.Responses;
+using MediatR;
+
+namespace Invoice.Application.Queries
+{
+    public class ReadItemCatalogQuery : IRequest<ItemCatalogResponse>
+    {
+        
+            public string Code { get; private set; }
+            public string CodeCatalog { get; private set; }
+
+            public ReadItemCatalogQuery(string code, string codeCatalog)
+            {
+                Code = code;
+                CodeCatalog = codeCatalog;
+            }
+        
+    }
+}
---
+++ Invoice/InvoiceUnach/Invoice.Application/Queries/ReadItemsCatalogQuery.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Invoice.Application.Dtos.Responses;
+using MediatR;
+
+namespace Invoice.Application.Queries
+{
+    public class ReadItemsCatalogQuery: IRequest<List<ItemCatalogResponse>>
+    {
+        public string Code { get; private  set; }
+        
+        public ReadItemsCatalogQuery(string code)
+        {
+            Code = code;
+        }
+      
+    }
+}
