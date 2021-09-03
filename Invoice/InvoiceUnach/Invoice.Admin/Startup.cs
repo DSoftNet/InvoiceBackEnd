4330965877b14365f6b93e983d3e8eb4db984da5
aasitimbay <alexasitimbay@outlook.com>
2021-09-02 21:32:51 -0500

    create view of users list

--- Invoice/InvoiceUnach/Invoice.Admin/Startup.cs
+++ Invoice/InvoiceUnach/Invoice.Admin/Startup.cs
@@ -1,13 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Autofac;
 using Invoice.Admin.Configuration;
 using Invoice.Infrastructure;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
