2c37b3296e7a00cb3bea4cc64f3ae6b773c2c12e
Jleone18 <lalangui1998@hotmail.com>
2021-09-07 17:38:35 -0500

    create view of subsidiary

--- Invoice/InvoiceUnach/Invoice.Admin/Controllers/SubsidiaryController.cs
+++ Invoice/InvoiceUnach/Invoice.Admin/Controllers/SubsidiaryController.cs
@@ -1,13 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Invoice.Admin.Models;
+using Invoice.Domain.Interfaces.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Invoice.Admin.Controllers
 {
     public class SubsidiaryController : Controller
     {
-        // GET
-        public IActionResult Index()
+        private readonly ILogger<SubsidiaryController> _logger;
+        private readonly ISubsidiaryRepository _subsidiaryRepository;
+
+        public SubsidiaryController(ILogger<SubsidiaryController> logger, ISubsidiaryRepository subsidiaryRepository)
         {
-            return View();
+            _logger = logger;
+            _subsidiaryRepository = subsidiaryRepository;
         }
+        
+        // GET
+        public async Task<IActionResult> Index(Guid userId)
+        {
+            return View(await GetSubsidiarys(userId));
+        }
+        
+        public async Task<IActionResult> LoadSubsidiary(Guid subsidiaryId)
+        {
+            var subsidiaryModel = new SubsidiaryModel();
+
+            subsidiaryModel.Option = "Edit";
+            
+            return View("Index", subsidiaryModel);
+        }
+
+        #region Private Methods
+        
+        private async Task<SubsidiaryModel> GetSubsidiarys (Guid userId)
+        {
+            var subsidiaryModel = new SubsidiaryModel();
+
+            subsidiaryModel.Option = "List";
+
+            subsidiaryModel.InputSubsidiarys = new List<SubsidiaryModel.InputSubsidiary>();
+
+            var subsidiarys = await _subsidiaryRepository.Get(userId);
+
+            foreach (var subsidiary in subsidiarys)
+            {
+                subsidiaryModel.InputSubsidiarys.Add(
+                    new SubsidiaryModel.InputSubsidiary(subsidiary.Id,subsidiary.Name,subsidiary.Address,
+                        subsidiary.Phone1,subsidiary.Phone2,subsidiary.UserId));
+            }
+
+            return subsidiaryModel;
+        }
+        
+        #endregion
+        
     }
 }
---
+++ Invoice/InvoiceUnach/Invoice.Admin/Models/SubsidiaryModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Invoice.Admin.Models
+{
+    public class SubsidiaryModel
+    {
+        [TempData] public string Option { get; set; }
+        
+        [BindProperty] public InputSubsidiary InputSubsidiarytModel { get; set; }
+        [BindProperty] public List<InputSubsidiary> InputSubsidiarys  { get; set; }
+
+        public class InputSubsidiary
+        {
+            public Guid Id { get; set; }
+            
+            [Required(ErrorMessage = "Campo obligatorio")]
+            [StringLength(50, ErrorMessage = "Digite máximo 30 caracteres.")] 
+            public string Name { get; private set; }
+            [Required(ErrorMessage = "Campo obligatorio")]
+            [StringLength(50, ErrorMessage = "Digite máximo 30 caracteres.")]
+            public string Address{ get; private set; }
+            [Required(ErrorMessage = "Campo obligatorio")]
+            [StringLength(50, ErrorMessage = "Digite máximo 10 caracteres.")]
+            public string Phone1 { get; private set; }
+            public string Phone2 { get; private set; }
+            public Guid UserId { get; private set; }
+            
+            public InputSubsidiary(Guid id,string name, string address, string phone1, string phone2, Guid userId)
+            {
+                Name = name;
+                Address = address;
+                Phone1 = phone1;
+                Phone2 = phone2;
+                UserId = userId;
+            }
+
+        }
+    }
+}
