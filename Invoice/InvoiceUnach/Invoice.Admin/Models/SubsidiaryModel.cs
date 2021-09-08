14cd566ce358c48bdc98b25f4eb085772f8e26f7
Jleone18 <lalangui1998@hotmail.com>
2021-09-08 09:58:47 -0500

    create view of subsidiary

--- Invoice/InvoiceUnach/Invoice.Admin/Models/SubsidiaryModel.cs
+++ Invoice/InvoiceUnach/Invoice.Admin/Models/SubsidiaryModel.cs
@@ -18,24 +18,17 @@ namespace Invoice.Admin.Models
             
             [Required(ErrorMessage = "Campo obligatorio")]
             [StringLength(50, ErrorMessage = "Digite máximo 30 caracteres.")] 
-            public string Name { get; private set; }
+            public string Name { get;  set; }
+            
             [Required(ErrorMessage = "Campo obligatorio")]
             [StringLength(50, ErrorMessage = "Digite máximo 30 caracteres.")]
-            public string Address{ get; private set; }
+            public string Address{ get;  set; }
+            
             [Required(ErrorMessage = "Campo obligatorio")]
             [StringLength(50, ErrorMessage = "Digite máximo 10 caracteres.")]
-            public string Phone1 { get; private set; }
-            public string Phone2 { get; private set; }
-            public Guid UserId { get; private set; }
-            
-            public InputSubsidiary(Guid id,string name, string address, string phone1, string phone2, Guid userId)
-            {
-                Name = name;
-                Address = address;
-                Phone1 = phone1;
-                Phone2 = phone2;
-                UserId = userId;
-            }
+            public string Phone1 { get;  set; }
+            public string Phone2 { get;  set; }
+            public Guid UserId { get;  set; }
 
         }
     }
